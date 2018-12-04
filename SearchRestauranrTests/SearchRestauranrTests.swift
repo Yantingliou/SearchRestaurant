9530d9536c8865faef0868a995b5f4a8638c8ee5
liouyanting <msze5189@gmail.com>
2018-12-04 17:36:54 +0800

    Initial Commit

---
+++ SearchRestauranrTests/SearchRestauranrTests.swift
@@ -0,0 +1,34 @@
+//
+//  SearchRestauranrTests.swift
+//  SearchRestauranrTests
+//
+//  Created by 劉彥廷 on 2018/12/4.
+//  Copyright © 2018年 liou. All rights reserved.
+//
+
+import XCTest
+@testable import SearchRestauranr
+
+class SearchRestauranrTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
